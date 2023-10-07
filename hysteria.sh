869b5917bd2d3495d9e1df1ee6ba4004e2ec01d7
yjlijiang <140770334+yjlijiangdepc@users.noreply.github.com>
2023-10-07 09:54:50 +0800

    Update hysteria.sh HY

--- hysteria.sh
+++ hysteria.sh
@@ -773,8 +773,8 @@ showconf(){
 menu() {
     clear
     echo "#############################################################"
-    echo -e "#                  ${BLUE} 一键安装hysteria ${PLAIN} 协议 ${PLAIN}                 #"
-    echo -e "# ${GREEN}作者修改${PLAIN}: YJLIJIANGDEPC  ${RED}(原脚本引用： MisakaNo の 小破站 ) ${PLAIN}                                #"
+    echo -e "# ${BLUE}                  一键安装hysteria ${YELLOW}协议 ${PLAIN}                 #"
+    echo -e "# ${GREEN}作者修改${PLAIN}: YJLIJIANGDEPC  ${RED}(原脚本引用： MisakaNo の 小破站 ) ${PLAIN}#"
     echo "#############################################################"
     echo ""
     echo -e " ${GREEN}1.${PLAIN} 安装 Hysteria1"
